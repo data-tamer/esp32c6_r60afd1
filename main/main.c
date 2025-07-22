c7834bbe3544f6d180f7cbd3a68a7ca8f7d9c863
riccardo <riccardo.ravaro@ubermorgen.land>
2025-07-22 12:58:17 +0700

    update fall

--- main/main.c
+++ main/main.c
@@ -734,7 +734,7 @@ void uart_read_task(void *arg)
                     if(control == 0x80 && command == 0x01 && payload_len == 1) {
                         // Human presence report
                         g_presence = (data[i+6] == 0x01);
-                        g_fall_alarm = gpio_get_level(FALL_ALARM_GPIO);
+                        g_fall_alarm = gpio_get_level(GPIO_NUM_18);
                         printf("Parsed Presence: %d\n", g_presence);
                     }
                     else if(control == 0x05 && command == 0x01 && payload_len == 1) {
@@ -763,7 +763,7 @@ void uart_read_task(void *arg)
                     else if(control == 0x83 && command == 0x01 && payload_len == 1) {
                         uint8_t fall_value = data[i + 6];
                         g_fall_alarm = (fall_value == 0x01);
-                        g_fall_alarm = gpio_get_level(FALL_ALARM_GPIO);
+                        g_fall_alarm = gpio_get_level(GPIO_NUM_18);
                         printf("Parsed Fall Alarm: %d\n", g_fall_alarm);
                     }
                     else if(control == 0x83 && command == 0x05 && payload_len == 1) {
